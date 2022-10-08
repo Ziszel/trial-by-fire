3142c5ec2de963a7082218564ea89a91155edc13
Ziszel <the8thnoteofficial@gmail.com>
2022-10-08 15:42:10 +0100

    Added 'win' comment to console and restart method to complete minimal game loop

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private float restartDelay = 2.0f;
     // Start is called before the first frame update
     public void EndGame()
     {
-        Debug.Log("Game over");
+        Debug.Log("You Won!");
+        Invoke("Restart", restartDelay);
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
